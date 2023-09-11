92a391da62ad5689db57f1021ca2a0b5d36ba585
Himanshu Shukla <133512069+HimanshuTechHelps@users.noreply.github.com>
2023-09-11 16:57:37 +0530

    Update SuperAdminDashboard.js

--- src/components/Pages/adminPages/SuperAdminDashboard.js
+++ src/components/Pages/adminPages/SuperAdminDashboard.js
@@ -1,9 +1,9 @@
 import React from "react";
 import RenderComponent from "../../customComponents/ComponentRenderer";
-import { AD_USER_DASHBOARD } from "../../../UserJson";
+import { AD_USER_DASHBOARD, CP_USER_DASHBOARD } from "../../../UserJson";
 
-export default function AdminDashboard() {
-
+export default function AdminDashboard({ role }) {
+console.log(role)
   return (
     <div style={{ marginTop: "100px" }}>
       <RenderComponent jsonToRender={AD_USER_DASHBOARD} />
