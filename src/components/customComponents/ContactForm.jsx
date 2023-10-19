ef36fa987c90c55ed17b4221fdff088c060e2e86
tanish-th <tanish@techhelps.co.in>
2023-10-19 10:06:22 +0530

    minor fix

--- src/components/customComponents/ContactForm.jsx
+++ src/components/customComponents/ContactForm.jsx
@@ -3,6 +3,10 @@ import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 function ContactForm() {
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div className='contact_us_comp' id='contact_us'>
             <div className='contact_us_wrapper'>
@@ -22,18 +26,18 @@ function ContactForm() {
                         <div className='contact_number addr_item'>+91 88045 04504</div>
                         <div className='contact_email addr_item'>contact@BuilderFloor.com</div>
                     </div>
-                    <form className='contact_form'>
-                        <input type="text" placeholder='Enter your name' className='contact_input oneline_input' />
+                    <form className='contact_form' onSubmit={handleFormSubmit}>
+                        <input type="text" placeholder='Enter your name' className='contact_input oneline_input' required />
                         <input type="text" placeholder='Company (optional)' className='contact_input oneline_input' />
-                        <input type="email" placeholder='Your email' className='contact_input oneline_input' />
-                        <input type="text" placeholder='Phone number' className='contact_input oneline_input' />
-                        <textarea id="" rows="10" placeholder='Tell us about yourself' className='contact_input multiline_input'></textarea>
+                        <input type="email" placeholder='Your email' className='contact_input oneline_input' required />
+                        <input type="tel" placeholder='Phone number' className='contact_input oneline_input' required />
+                        <textarea id="" rows="10" placeholder='Tell us about yourself' className='contact_input multiline_input' required></textarea>
                         <div className='form_controls'>
                             <button type='submit' className='contact_submit'>
                                 <span className='btn_label'>Send Message</span>
                                 <ArrowForwardIcon className='btn_icon' />
                             </button>
-                            <p className='contact_note'>By clicking Send Message button, you agree our terms and policy.</p>
+                            <p className='contact_note'>By clicking Send Message button, you agree to our terms and policy.</p>
                         </div>
                     </form>
                 </div>
