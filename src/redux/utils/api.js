9af25991a38a0dd13c6a377769f0cd36398f7800
ArijitPatra2906 <patraarijit440@gmail.com>
2024-02-29 13:41:25 +0530

    baseurl change

--- src/redux/utils/api.js
+++ src/redux/utils/api.js
@@ -1,7 +1,7 @@
 import _ from "lodash";
 
 export const APP_DOMAIN = "https://builderfloor.com";
-export const API_DOMAIN = "https://bfservices.onrender.com/api/";
+export const API_DOMAIN = "https://bfservices.trainright.fit/api/";
 const CHAT_API_DOMAIN = "https://itsolutionshub.com/chat";
 // const API_DOMAIN = "http://localhost:5000/api/";
 
